88e59dcfcedbbfcb67c2ee563dad4fc296c0815e
Han Yi <hanyiwindows@live.com>
2024-02-09 19:31:49 +0800

    feat(upload):  trial upload CORS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState,ChangeEvent} from "react";
 
 function App() {
     const [value, setValue]= useState('')
+    const [uploadedFile, setUploadedFile] = useState(null);
+    const [uploadResult, setUploadResult] = useState(null);
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        console.log('file changed');
+        setUploadedFile(file);
+    };
+
+    const handleUpload = async () => {
+        console.log("starting upload");
+        if (!uploadedFile) {
+            console.log('No file selected');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', uploadedFile);
+
+        try {
+            const response = await fetch('https://contosowebbackend20240207112230.azurewebsites.net/api/first/OnPostUpload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                console.log('File uploaded successfully');
+            } else {
+                console.error('Failed to upload file');
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    };
 
     // get dom
     const inputRef = useRef(null)
@@ -13,15 +46,19 @@ function App() {
         <div className="App">
             <input
                 ref={inputRef}
-                value={value} onChange={(e)=>setValue(e.target.value)}
+                value={value}
                 type="text" />
-            <button onClick={showDom}>click im new button me</button>
-
+            <button onClick={handleUpload}>click im new button me</button>
+            <input type="file" onChange={handleFileChange} accept="image/gif,image/jpeg,image/jpg,image/png" multiple/>
         </div>
 
 
 
     );
+
+
+
+
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
  //import App from './App.jsx'
 import DemoApp from "./DemoApp.jsx";
 import './index.css'
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DemoApp />
+    <App />
   </React.StrictMode>,
 )
