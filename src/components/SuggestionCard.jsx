4097c6d8dd35ca6916c6e14d705d7c6d2682eaf3
Jacob Avalanchel <hanyiwindows@live.com>
2024-02-27 16:43:45 +0800

    feat(SuggestionPage): ADD

--- src/components/SuggestionCard.jsx
+++ src/components/SuggestionCard.jsx
@@ -3,23 +3,21 @@ import { FaStar } from "react-icons/fa";
 import toast from "react-hot-toast";
 
 
-const ProductCard = ({id, name, price, imgpath, description}) =>{
+const SuggestionCard = ({id, title, content, fwlink}) =>{
     return (
         <div className=" flex flex-col w-full lg:w-2/6 border-2 border-DarkColor p-3 rounded-lg gap-5 hover:bg-PrimaryColor transition duration-300 ease-in-out cursor-pointer">
             <div>
                 <ImQuotesLeft size={25} />
                 <h1 className=" text-xl font-semibold text-ExtraDarkColor pt-4">
-                    {name}
+                    {title}
                 </h1>
             </div>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-                consequatur fuga suscipit perspiciatis totam. Eos esse nisi omnis dicta,
-                aperiam distinctio fuga ipsam rerum rem.
+                {content}
             </p>
         </div>
     );
 
 
 }
-export default ProductCard;
+export default SuggestionCard;
