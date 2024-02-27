7dbabecac9f0be15623a7fbbd66649cd7b3e3287
Jacob Avalanchel <hanyiwindows@live.com>
2024-02-27 16:41:45 +0800

    feat(SuggestionPage): ADD

--- src/SuggestionPage.jsx
+++ src/SuggestionPage.jsx
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
-
+import SuggestionCard from './components/SuggestionCard.jsx'
 const SuggestionPage = () => {
 
     return (
-        <div>
+        <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5">
 
+            {/* heading section */}
+            <h1 className=" font-semibold text-center text-ExtraDarkColor text-4xl lg:mt-14 mt-24 ">
+                Feedback Corner
+            </h1>
+
+            {/* review card section */}
+            <div className=" flex flex-col items-center xl:flex-row gap-5 justify-center py-4 my-8">
+                <SuggestionCard name="Emily Davis"/>
+                <SuggestionCard name="Madison Mitchell"/>
+                <SuggestionCard name="Aubrey Carter"/>
+            </div>
         </div>
     );
 };
