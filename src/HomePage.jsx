1d75d54e9312a189c5642f06e345fc38fdced0dd
Han Yi <hanyiwindows@live.com>
2024-02-28 12:40:24 +0800

    feat(MyPage): init

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -39,50 +39,50 @@ const HomePage = () => {
     };
     return (
         <div>
-                <div className="min-h-screen px-2 flex flex-col justify-center my-20">
-                    {/* heading */}
-                    <h1 className="font-semibold text-3xl text-center text-black">
-                        主页
-                    </h1>
-                    <div
-                        className="h-[370px] 2xl:h-[420px] text-black flex flex-col justify-around items-center rounded-lg shadow-lg mt-10 mb-20 cursor-pointer bg-lime-50 hover:bg-lime-100">
-                        {/*<div>*/}
-                        {/*    <img*/}
-                        {/*        src=""*/}
-                        {/*        alt="img"*/}
-                        {/*        className="h-56 2xl:h-64 rounded-t-xl w-full"*/}
-                        {/*    />*/}
-                        {/*</div>*/}
-                        <h2 className=" font-normal text-2xl text-center text-black">
-                            拍照食品健康助手
-                        </h2>
-                        <div className="w-3/4 flex flex-row gap-2 justify-center items-center">
+            <div className="min-h-screen px-2 flex flex-col justify-center my-20">
+                {/* heading */}
+                <h1 className="font-semibold text-3xl text-center text-black">
+                    主页
+                </h1>
+                <div
+                    className="h-[370px] 2xl:h-[420px] text-black flex flex-col justify-around items-center rounded-lg shadow-lg mt-10 mb-20 cursor-pointer bg-lime-50 hover:bg-lime-100">
+                    {/*<div>*/}
+                    {/*    <img*/}
+                    {/*        src=""*/}
+                    {/*        alt="img"*/}
+                    {/*        className="h-56 2xl:h-64 rounded-t-xl w-full"*/}
+                    {/*    />*/}
+                    {/*</div>*/}
+                    <h2 className=" font-normal text-2xl text-center text-black">
+                        拍照食品健康助手
+                    </h2>
+                    <div className="w-3/4 flex flex-row gap-2 justify-center items-center">
 
-                            <Button
-                                variant="contained"
-                                component="label"
-                            >
-                                选择文件
-                                <input
-                                    hidden
-                                    type="file"
-                                    onChange={handleFileChange}
-                                    accept="image/gif,image/jpeg,image/jpg,image/png"
-                                    multiple
-                                />
-                            </Button>
-                            <Button
-                                variant="outlined"
-                                className="bg-lime-300 hover:bg-lime-400 active:bg-lime-500 text-black px-4 py-2 font-medium "
-                                onClick={handleUpload}
-                            >
-                                上传所选
-                            </Button>
+                        <Button
+                            variant="contained"
+                            component="label"
+                        >
+                            选择文件
+                            <input
+                                hidden
+                                type="file"
+                                onChange={handleFileChange}
+                                accept="image/gif,image/jpeg,image/jpg,image/png"
+                                multiple
+                            />
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            className="bg-lime-300 hover:bg-lime-400 active:bg-lime-500 text-black px-4 py-2 font-medium "
+                            onClick={handleUpload}
+                        >
+                            上传所选
+                        </Button>
 
 
-                        </div>
                     </div>
                 </div>
+            </div>
         </div>
     );
 };
--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,12 +1,27 @@
 import React, {useState} from 'react';
+import {Avatar, Button} from "@mui/material";
 
 const MyPage = () => {
 
     return (
-        <div>
+
+        <div className="flex flex-col justify-center -mt-4">
+            {/* heading */}
+            <div>
+                <div
+                    className="h-[10rem] 2xl:h-[15rem] text-black flex flex-col justify-around items-center rounded-xl shadow-xl cursor-pointer bg-lime-50 hover:bg-lime-100">
+                    <h1 className="font-semibold text-3xl text-center text-black">
+                        我
+                    </h1>
+                    <Avatar alt="Personal icon" src="/src/assets/react.svg" />
+                    <div className="w-3/4 flex flex-row gap-2 justify-center items-center">
 
 
 
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
