db54f21c6f1bd90215b411aca868a4ab6ef67831
Han Yi <hanyiwindows@live.com>
2024-02-29 10:08:32 +0800

    feat(MarketPage): update mobile market layout

--- src/MarketPage.jsx
+++ src/MarketPage.jsx
@@ -4,43 +4,45 @@ const MarketPage = () => {
     const shopItems = [
         {
             id: 6,
-            img: "/src/assets/img/product7.jpg",
-            name: "FrostFire Outerlayers",
-            price: "398.00",
+            img: "/src/assets/img/product1.png",
+            name: "健康黄皮水果",
+            price: "34.00",
             discount: "24% Off",
+            description:"消化不良"
         },
         {
             id: 7,
-            img: "/src/assets/img/product8.jpg",
-            name: "DaintyDoll Dresses",
+            img: "/src/assets/img/product2.png",
+            name: "生态莲雾",
             price: "258.00",
             discount: "40% Off",
+            description:"精神压力大"
         },
         {
             id: 8,
-            img: "/src/assets/img/product9.jpg",
-            name: "TrendTraverse Bag",
+            img: "/src/assets/img/product3.png",
+            name: "富硒地瓜",
             price: "104.00",
             discount: "35% Off",
         },
         {
             id: 9,
-            img: "/src/assets/img/product10.jpg",
-            name: "Fitness Footgear",
+            img: "/src/assets/img/product4.png",
+            name: "特色菠萝蜜",
             price: "299.00",
             discount: "40% Off",
         },
         {
             id: 10,
-            img: "/src/assets/img/product11.jpg",
-            name: "UrbanBelle Hoodies",
+            img: "/src/assets/img/product5.png",
+            name: "养生枸杞",
             price: "129.00",
             discount: "28% Off",
         },
         {
             id: 11,
-            img: "/src/assets/img/product12.jpg",
-            name: "Elite Handbags",
+            img: "/src/assets/img/product6.png",
+            name: "野生灵芝",
             price: "189.00",
             discount: "30% Off",
         },
@@ -50,13 +52,13 @@ const MarketPage = () => {
         <div className="min-h-screen flex flex-col justify-center my-20">
             {/* heading */}
             <h1 className="font-semibold text-3xl text-center text-black">
-                健康市场
+                健康助农市场
             </h1>
             {/* card flow */}
             <div className="flex flex-wrap md:flex-wrap justify-center md:pt-8">
                 {shopItems.map((item) =>
                     (
-                    <div key={item.id} className="w-2/4 md:w-2/4 lg:w-1/4">
+                    <div key={item.id} className="w-2/4 md:w-1/3 lg:w-1/4">
                 <ProductCard key={item.id}
                              id={item.id}
                              imgpath={item.img}
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
 import toast from "react-hot-toast";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import {IconButton} from "@mui/material";
 
+const ProductCard = ({id, name, price, imgpath, description}) => {
+    const addToCart = new Promise((resolve, reject) => {
 
-const ProductCard = ({id, name, price, imgpath, description}) =>{
-    const addToCart = new Promise((resolve,reject) =>{
-
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log("resolved");
             resolve();
 
-        },2000);
+        }, 2000);
 
     })
-    const addToCartOnTap=()=>{
+    const addToCartOnTap = () => {
         // console.log("tapped");
         // toast.promise(addToCart,
         //     {loading: 'Loading',
@@ -22,35 +22,27 @@ const ProductCard = ({id, name, price, imgpath, description}) =>{
         toast.success("已添加购物车")
     }
 
-    return(
-        <div className="h-[370px] 2xl:h-[420px] text-black rounded-lg shadow-lg m-1 cursor-pointer bg-lime-50 hover:bg-lime-100"
+    return (
+        <div className="text-black rounded-lg shadow-lg m-1 cursor-pointer bg-lime-50 hover:bg-lime-100"
              key={id}
         >
-            <div>
+
+            <div className=" flex flex-col justify-center items-center">
                 <img
                     src={imgpath}
                     alt="img"
-                    className="h-56 2xl:h-64 rounded-t-xl w-full"
+                    className="h-[10rem] md:h-[20rem] rounded-t-xl w-full object-cover"
                 />
-            </div>
-            <div className=" flex flex-col justify-center items-center">
-                <h1 className=" font-semibold text-xl pt-4">{name}</h1>
-                <div className=" flex gap-10 pt-2">
-                    <h2 className=" font-medium text-lg ">${price}</h2>
-                    <h2 className=" font-medium text-DarkColor">{description}</h2>
-                </div>
 
-                <div className=" flex gap-8 pt-4">
-                    <button
-                        className=" bg-lime-300 hover:bg-lime-400 active:bg-lime-500 text-black px-4 py-2 font-medium "
-                        onClick={addToCartOnTap}
-                    >
-                       添加至购物车
-                    </button>
-                    <div className=" flex gap-1 items-center">
-                        <h2>5.0</h2>
-                        <FaStar className=" text-yellow-300"/>
+                <div className="gap-1 flex flex-col items-center justify-center">
+                    <h1 className=" font-semibold text-lg pt-4">{name}</h1>
+                    <div className="flex flex-row items-center justify-center">
+                        <h2 className=" font-medium text-lg ">￥{price}</h2>
+                        <IconButton color="primary" aria-label="add to shopping cart" onClick={addToCartOnTap}>
+                            <AddShoppingCartIcon/>
+                        </IconButton>
                     </div>
+
                 </div>
             </div>
         </div>
