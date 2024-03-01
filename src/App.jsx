561a5bdeb61e54486b686e2f1f70b807f6c63bcc
Han Yi <hanyiwindows@live.com>
2024-03-01 20:30:54 +0800

    fix(App): bootstrap bar reset value while redirecting

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React, {useRef, useState, ChangeEvent} from "react";
 import './index.css'
 import {Container, Button, TextField, CssBaseline, Box} from "@mui/material";
 import Navbar from "./Navbar";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Link,
+    useNavigate
+} from "react-router-dom";
 import {BottomNavigation, BottomNavigationAction} from "@mui/material";
 import Topbar from "./Appbar.jsx"
 import HomeIcon from "@mui/icons-material/Home";
@@ -15,10 +21,11 @@ import MyPage from "./MyPage.jsx";
 import SuggestionPage from "./SuggestionPage.jsx"
 import {Toaster} from "react-hot-toast";
 
-
 function App() {
+    const navigate=useNavigate();
     const [currentTab, setCurrentTab] = React.useState(0);
     return (
+        // <Router>
         <React.Fragment>
             <CssBaseline/>
             {/*//data binding*/}
@@ -29,28 +36,32 @@ function App() {
                 position="top-center"
             />
             <div className="App pb-10">
-                <Router>
+
                     <Routes>
                         <Route path="/" exact element={<HomePage/>}/>
                         <Route path="/about" element={<SuggestionPage/>}/>
                         <Route path="/services" element={<MarketPage/>}/>
                         <Route path="/contact" element={<MyPage/>}/>
                     </Routes>
-                </Router>
+
             </div>
-                <BottomNavigation
-                    className="bottomNavigation"
-                    value={currentTab}
-                    onChange={(event,newValue)=>{setCurrentTab(newValue);console.log(newValue);}}
-                >
-                    <BottomNavigationAction label="Home" icon={<HomeIcon/>} href="/"/>
-                    <BottomNavigationAction label="About" icon={<InfoIcon/>} href="/about"/>
-                    <BottomNavigationAction label="Services" icon={<WorkIcon/>} href="/services"/>
-                    <BottomNavigationAction label="Contact" icon={<ContactMailIcon/>} href="/contact"/>
-                </BottomNavigation>
+            <BottomNavigation
+                className="bottomNavigation"
+                value={currentTab}
+                onChange={(event, newValue) => {
+                    setCurrentTab(newValue);
+                    console.log(newValue);
+                }}
+            >
+                <BottomNavigationAction label="Home" icon={<HomeIcon/>} onClick={()=>navigate("/home")}/>
+                <BottomNavigationAction label="About" icon={<InfoIcon/>} onClick={()=>navigate("/about")}/>
+                <BottomNavigationAction label="Services" icon={<WorkIcon/>} onClick={()=>navigate("/services")}/>
+                <BottomNavigationAction label="Contact" icon={<ContactMailIcon/>} onClick={()=>navigate("/contact")}/>
+            </BottomNavigation>
 
         </React.Fragment>
-    );
+        // </Router>
+);
 }
 
 export default App;
