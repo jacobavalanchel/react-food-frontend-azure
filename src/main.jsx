a4f619bfcedabdea5410b393fcab7fda55e5bfcb
Han Yi <hanyiwindows@live.com>
2024-04-22 23:58:58 +0800

    feat(main): router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
- //import App from './App.jsx'
-
 import App from "./App.jsx";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./HomePage.jsx";
+import SuggestionPage from "./SuggestionPage.jsx";
+import MarketPage from "./MarketPage.jsx";
+import MyPage from "./MyPage.jsx";
+import {CssBaseline} from "@mui/material";
+
+const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <App/>,
+            children: [
+                {
+                    path: "/home",
+                    element:<HomePage/>,
+                },
+                {
+                    path: "/about",
+                    element:<SuggestionPage/>,
+                },
+                {
+                    path: "/services",
+                    element:<MarketPage/>,
+                },
+                {
+                    path: "/contact",
+                    element:<MyPage/>,
+                }
+            ]
+        }
+    ]
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-
+      <CssBaseline/>
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
