adf8dbf509270d6d1bbbdabf05634c416ada1dbe
Han Yi <hanyiwindows@live.com>
2024-04-27 00:48:44 +0800

    style(main): reformat

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./HomePage.jsx";
 import SuggestionPage from "./SuggestionPage.jsx";
 import MarketPage from "./MarketPage.jsx";
 import MyPage from "./MyPage.jsx";
-import {CssBaseline} from "@mui/material";
-import CollapsibleTable from "./newForm.jsx";
+import { CssBaseline } from "@mui/material";
+import CollapsibleTable from "./components/DetailForm.jsx";
 
 const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <App/>,
-            children: [
-                {
-                    path: "/home",
-                    element:<HomePage/>,
-                },
-                {
-                    path: "/about",
-                    element:<SuggestionPage/>,
-                },
-                {
-                    path: "/services",
-                    element:<MarketPage/>,
-                },
-                {
-                    path: "/contact",
-                    element:<MyPage/>,
-                }
-            ]
-        }
-    ]
-);
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <SuggestionPage />,
+      },
+      {
+        path: "/services",
+        element: <MarketPage />,
+      },
+      {
+        path: "/contact",
+        element: <MyPage />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <CssBaseline/>
-      {/*<RouterProvider router={router}/>*/}
-      <CollapsibleTable/>
+    <CssBaseline />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
